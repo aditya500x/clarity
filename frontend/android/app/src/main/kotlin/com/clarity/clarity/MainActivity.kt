f5059c9c991976a3820a7081498923a155923c01
aditya500x <aditya500897@gmail.com>
2026-01-26 14:02:32 +0530

    clean frontend

--- frontend/android/app/src/main/kotlin/com/clarity/clarity/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.clarity.clarity
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- frontend/lib/viewmodels/home_viewmodel.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class HomeViewModel extends ChangeNotifier {
-  // Navigation logic or other state can go here
-  // For now, it mostly handles business logic for the home view
-}
